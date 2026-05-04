a35136c7d9f3376838cd4134bf4e26fa52f207d8
Josh-Hiz <jhizgiaev2704@gmail.com>
2026-05-04 00:16:14 -0400

    minor edits

--- test/main.cpp
+++ test/main.cpp
@@ -1,15 +1,14 @@
+#include "../include/lockless/lockless.h"
 #include <atomic>
 #include <iostream>
 #include <thread>
+
+lockless::SPSCQueue<int> queue(1024);
 
 static std::atomic<int> counter{0};
 static std::atomic<bool> ready{false};
 static int data = 0;
 
-// memory_order_relaxed just says:
-// "Just do the atomic operation. I don't care about
-// ordering relative to anything else". In other words
-// it doesn't matter if instructions are reordered or not here
 static void task_1() {
     for (int i = 0; i < 1000; i++) {
         counter.fetch_add(1, std::memory_order_relaxed);
@@ -21,6 +20,24 @@ static void task_2_1() {}
 static void task_2_2() {}
 
 int main() {
+    // Producer thread
+    std::thread producer([&] {
+        for (int i = 0; i < 1000000; ++i)
+            while (!queue.push(i)) {
+                std::this_thread::yield();
+            }
+    });
+
+    // Consumer thread
+    std::thread consumer([&] {
+        int count = 0;
+        while (count < 1000000) {
+            if (auto val = queue.pop()) {
+                std::cout << *val << std::endl;
+                ++count;
+            }
+        }
+    });
     std::thread t1(task_1);
     std::thread t2(task_1);
 
