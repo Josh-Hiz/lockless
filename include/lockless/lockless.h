7fd60110051fdf29054f801ff5ea95e80b7343d8
Josh-Hiz <jhizgiaev2704@gmail.com>
2026-05-04 00:15:55 -0400

    add a one-header-only option for lockless

---
+++ include/lockless/lockless.h
@@ -0,0 +1,14 @@
+#ifndef _LOCKLESS_H_
+#define _LOCKLESS_H_
+
+#include "containers/mpmc_queue.h"
+#include "containers/spsc_queue.h"
+#include "core/backoff.h"
+#include "core/cache.h"
+#include "core/ebr.h"
+#include "core/memory.h"
+#include "executors/task.h"
+#include "executors/thread_pool.h"
+#include "sync/seqlock.h"
+
+#endif
