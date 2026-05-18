69181057d48104c4444634d5001e1dd17e51fcbe
Josh-Hiz <jhizgiaev2704@gmail.com>
2026-05-17 21:25:37 -0400

    Update upper-level lockless.h header

--- include/lockless/lockless.h
+++ include/lockless/lockless.h
@@ -1,14 +1,21 @@
-#ifndef _LOCKLESS_H_
-#define _LOCKLESS_H_
+#ifndef LOCKLESS_H
+#define LOCKLESS_H
 
-#include "containers/mpmc_queue.h"
-#include "containers/spsc_queue.h"
-#include "core/backoff.h"
-#include "core/cache.h"
-#include "core/ebr.h"
-#include "core/memory.h"
-#include "executors/task.h"
-#include "executors/thread_pool.h"
-#include "sync/seqlock.h"
+// Include lockless core concurrency primitives
+#include "core/lockless_backoff.h"
+#include "core/lockless_cache.h"
+#include "core/lockless_ebr.h"
 
-#endif
+// Include all container classes
+#include "containers/lockless_mpmc_queue.h"
+#include "containers/lockless_spsc_queue.h"
+#include "containers/lockless_stack.h"
+
+// Include the threadpool and task classes
+#include "executors/lockless_task.h"
+#include "executors/lockless_fork_join_pool.h"
+
+// Include synchronization primitives
+#include "sync/lockless_seqlock.h"
+
+#endif // LOCKLESS_H
